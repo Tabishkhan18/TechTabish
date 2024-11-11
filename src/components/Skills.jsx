beee71cff4a64f1c13bb049f01ea4822b820af0c
Tabishkhan18 <tabishkhan1811@gmail.com>
2024-11-11 11:32:22 +0530

    update skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ const Skills = () => {
     return (
         <div className='bg-neutral-50 flex justify-between md:p-20 p-7' id='skills'>
             <div className="skillheading hidden md:flex w-1/2">
-                <img src="./skills4.jpg" alt="image" className='w-[40rem] rounded-3xl' />
+                <img src="./skills4.jpg" alt="image" className='w-[40rem] rounded-3xl grayscale-[50%]' />
             </div>
             <div className="skills md:ps-28 md:w-1/2">
                 <h1 className="heading py-10 font-semibold text-xl">
